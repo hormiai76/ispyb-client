aabfe00e9c3649f0e354696b0d15fe15051390b8
Alberto Nardella <Alberto.Nardella@maxiv.lu.se>
2019-10-22 18:22:09 +0200

    Adding a new script to update a proposal from RabbitMQ

--- python/userportal/MAXIVReceiver.py
+++ python/userportal/MAXIVReceiver.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import pika
+import traceback
+import sys
+from suds.client import Client
+from suds.transport.http import HttpAuthenticated
+import os, shutil
+from contextlib import closing
+from distutils.util import strtobool
+from datetime import datetime,timedelta
+from random import randint
+import time
+import base64
+import ConfigParser
+from operator import itemgetter
+import requests
+import myLogger
+import json
 
 def authenticate(url, user, password, site, proxies):
     r = requests.post(url + '/authenticate?site=' + site, headers={'content-type': 'application/x-www-form-urlencoded'}, proxies=proxies, data={'login': user, 'password': password}, verify=False)
@@ -16,7 +32,19 @@ def updatebyproposal(token, proposal):
     r = requests.post(url + '/' +token + '/proposal/{proposal}/update', headers={'content-type': 'application/x-www-form-urlencoded'}, proxies=proxies, data=payload, verify=False)
     print(r.text)
 
-
+def callback(ch, method, properties, body):
+    proposalNumber = "";
+    print body
+    if "Update proposal:" in body:
+	token = authenticate(url, user, password, site, proxies)
+	if token is None:
+		print("Token can not be None")
+		sys.exit()
+        a = body.split(":")
+        proposalNumber = a[1].strip()
+	proposal = "MX" +proposalNumber
+	updatebyproposal(token, proposal)
+    print(" [x] Proposal to update %r" % proposalNumber )
 
 if __name__ == "__main__":
 
@@ -48,31 +76,21 @@ if __name__ == "__main__":
 
         rabbitmqUser = str(credentialsConfig.get('Credential', 'rabbitmqUser'))
         rabbitmqPassword = str(credentialsConfig.get('Credential', 'rabbitmqPassword'))
-        rabbitmqHost = str(config.get('Connection', 'url_rabbitmq_host'))
-        rabbitmqPort = str(config.get('Connection', 'url_rabbitmq_port'))
-
-credentials = pika.PlainCredentials(rabbitmqUser, rabbitmqPassword)
-parameters = pika.ConnectionParameters(rabbitmqHost, rabbitmqPort
+        rabbitmqHost = str(config.get('RabbitMQ', 'host'))
+        rabbitmqPort = str(config.get('RabbitMQ', 'port'))
+        rabbitmqQueue = str(config.get('RabbitMQ', 'queue'))
+        credentials = pika.PlainCredentials(rabbitmqUser, rabbitmqPassword)
+        parameters = pika.ConnectionParameters(rabbitmqHost, rabbitmqPort
                                    rabbitmqPort,
                                    '/',
                                    credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
 
-channel.queue_declare(queue='hello')
-
-
-def callback(ch, method, properties, body):
-    proposalNumber = "";
-    print body
-    if "Update proposal:" in body:
-        a = body.split(":")
-        proposalNumber = a[1].strip()
-    print(" [x] Proposal to update %r" % proposalNumber )
+        channel.queue_declare(queue=rabbitmqQueue)
 
 
-channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
+	channel.basic_consume(queue=rabbitmqQueue, on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+	print(' [*] Waiting for messages. To exit press CTRL+C')
+	channel.start_consuming()
